3d27da503c0bc839da153640bb045b84fdf624e8
root <root@DESKTOP-V8P4AHJ.localdomain>
2021-01-28 20:03:26 +0200

    added new form and extra features

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// POPOVERS
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+
 // INPUT TYPE='COLOR'
 var colorValue = document.getElementById("favcolor");
 var textColor = document.querySelector(".color-text");
